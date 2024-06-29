4ddcc8f403efcfcecba0a4813c63dd377d4ee16f
deval <deval.patel5@dcmail.ca>
2024-06-29 15:11:42 -0400

    Created Queue using LinkedList

---
+++ MyQueue.py
@@ -0,0 +1,40 @@
+from MyLinkedList import Node
+
+
+class QueueList:
+    def __init__(self):
+        self.rear = None
+        self.front = None
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.front is None:
+            self.rear = new_node
+            self.front = self.rear
+        else:
+            self.rear.next = new_node
+            self.rear = new_node
+
+    def dequeue(self):
+        if self.front is None:
+            print("Empty Queue")
+        else:
+            self.front = self.front.next
+
+    def  front_item(self):
+        if self.front is not None:
+            return self.front.data
+
+    def  rear_item(self):
+        if self.front is not None:
+            return self.rear.data
+
+    def traverse(self):
+        if self.front is None:
+            print("Empty Queue")
+        else:
+            current = self.front
+            while current is not None:
+                print(current.data, end=" ")
+                current = current.next
+
