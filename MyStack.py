09ffe2319294e4a0cacb48180dde9112d83aa4ba
deval <deval.patel5@dcmail.ca>
2024-07-14 13:57:20 -0400

    reformat file

--- MyStack.py
+++ MyStack.py
@@ -5,20 +5,15 @@ class Stack:
 
     def __init__(self):
         self.top = None
-        self.n = 0
-
-    def __len__(self):
-        return self.n
 
     def isEmpty(self):
-        return self.n == 0
+        return self.top is None
 
     def push(self, value):
         newNode = Node(value)
 
         newNode.next = self.top
         self.top = newNode
-        self.n += 1
 
     def peek(self):
         if self.isEmpty():
@@ -31,17 +26,11 @@ class Stack:
             return "Empty Stack"
         else:
             self.top = self.top.next
-            self.n -= 1
-
-    def clear(self):
-        self.top = None
-        self.n = 0
 
     def traverse(self):
         if self.isEmpty():
             return "Empty Stack"
         current = self.top
         while current is not None:
-            print(current.data)
+            print(current.data, end=" ")
             current = current.next
-
