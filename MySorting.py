0b94cc2c993be160bdaa09bd3cf74bc7452f67c6
deval <deval.patel5@dcmail.ca>
2024-07-29 13:57:12 -0400

    bubble sort

---
+++ MySorting.py
@@ -0,0 +1,13 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        swapped = False
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j] = arr[j] + arr[j + 1]
+                arr[j + 1] = arr[j] - arr[j + 1]
+                arr[j] = arr[j] - arr[j + 1]
+                swapped = True
+
+        if not swapped:
+            break
